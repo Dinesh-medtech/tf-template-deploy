b01146facecd66593d6085c784aa7e168af2e1ac
Dinesh-medtech <dineshkumar@medtechsoft.co>
2024-10-07 16:11:11 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,77 +1,166 @@
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1" # Adjust based on your requirements
 }
 
-resource "aws_iam_role" "cognito_role" {
-  name = "cognito_user_pool_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "cognito-idp.amazonaws.com"
-        }
-        Effect    = "Allow"
-      },
-    ]
-  })
+variable "environment_name" {
+  description = "Environment name for the deployment"
+  type        = string
 }
 
-resource "aws_iam_role_policy_attachment" "cognito_policy_attachment" {
-  role       = aws_iam_role.cognito_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/CognitoUserPool"
-
-  depends_on = [aws_iam_role.cognito_role]
+variable "google_client_id" {
+  description = "Google Client ID"
+  type        = string
 }
 
-resource "aws_cognito_user_pool" "my_user_pool" {
-  name = "my-user-pool"
+variable "google_client_secret" {
+  description = "Google Client Secret"
+  type        = string
+}
 
-  iam_role = aws_iam_role.cognito_role.arn
+variable "linkedin_client_id" {
+  description = "LinkedIn Client ID"
+  type        = string
+}
 
-  username_attributes = ["email"]
+variable "linkedin_client_secret" {
+  description = "LinkedIn Client Secret"
+  type        = string
+}
 
-  schema {
-    name     = "email"
-    required = true
-    mutable  = false
-    attribute_data_type = "String"
+# Cognito User Pool
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "AcabotNext-var.environment_name"
+
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+
+  auto_verified_attributes = ["email"]
+  username_attributes       = ["email"]
+
+  password_policy {
+    minimum_length               = 8
+    require_lowercase            = true
+    require_numbers              = true
+    require_symbols              = true
+    require_uppercase            = true
+    temporary_password_validity_days = 30
   }
 }
 
-resource "aws_cognito_user_pool_client" "my_user_pool_client" {
-  name         = "my-user-pool-client"
-  user_pool_id = aws_cognito_user_pool.my_user_pool.id
+# Cognito User Pool Resource Server
+resource "aws_cognito_resource_server" "resource_server" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  identifier   = "acabotnext_api"
+  name         = "AcabotNext-var.environment_name"
 
-  allowed_oauth_flows       = ["code"]
-  allowed_oauth_scopes      = ["phone", "email", "openid", "profile"]
-  callback_urls              = ["https://example.com/callback"]
-  logout_urls                = ["https://example.com/logout"] 
+  scope {
+    scope_name        = "all"
+    scope_description = "All resources"
+  }
 }
 
-resource "aws_db_instance" "my_db" {
-  identifier        = "my-database"
-  engine            = "postgres"
-  instance_class    = "db.t3.micro"
-  allocated_storage  = 20
-  username          = "myuser" 
-  password          = "mypassword" 
-  db_name           = "mydatabase"
-  skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
+# Cognito User Pool Client for Social Login
+resource "aws_cognito_user_pool_client" "user_pool_client1" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  name         = "AcabotNext-SocialLogin-var.environment_name"
+
+  generate_secret = true
+  allowed_oauth_flows = ["code"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes = [
+    "openid",
+    "profile",
+    "email",
+    "aws_cognito_resource_server.resource_server.identifier/all"
+  ]
+
+  explicit_auth_flows = ["USER_PASSWORD_AUTH"]
+
+  callback_urls = [
+    "http://localhost:3000/api/auth/callback/cognito",
+    "http://localhost:3000/api/auth/callback/google",
+    "http://localhost:3000/api/auth/callback/linkedin",
+    "http://localhost:3000/dashboard/api/auth/callback",
+    "http://localhost:3000/dashboard",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/api/auth/callback",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/api/auth/callback/cognito",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/api/auth/callback/google",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/api/auth/callback/linkedin",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/dashboard"
+  ]
+
+  logout_urls = [
+    "http://localhost:3000/login",
+    "https://${lookup(data.aws_ssm_parameter.domain_map, var.environment_name)}/login"
+  ]
+
+  supported_identity_providers = ["LinkedIn", "Google"]
 }
 
+# Cognito User Pool Client for Credential Login
+resource "aws_cognito_user_pool_client" "user_pool_client2" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  name         = "AcabotNext-CredentialLogin-var.environment_name"
+
+  generate_secret = false
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
+
+# Google Identity Provider
+resource "aws_cognito_user_pool_identity_provider" "google_identity_provider" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  provider_name = "Google"
+  provider_type = "Google"
+
+  provider_details = {
+    client_id     = var.google_client_id
+    client_secret = var.google_client_secret
+    authorize_scopes = "email"
+  }
+
+  attribute_mapping = {
+    username = "sub"
+    email    = "email"
+  }
+}
+
+# LinkedIn Identity Provider
+resource "aws_cognito_user_pool_identity_provider" "linkedin_identity_provider" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  provider_name = "LinkedIn"
+  provider_type = "OIDC"
+
+  provider_details = {
+    attributes_request_method = "GET"  
+    authorize_url             = "https://www.linkedin.com/oauth/v2/authorization"
+    client_id                 = var.linkedin_client_id
+    client_secret             = var.linkedin_client_secret
+    oidc_issuer               = "https://www.linkedin.com/oauth"
+    authorize_scopes          = "openid profile email"
+    token_url                 = "https://www.linkedin.com/oauth/v2/accessToken"
+    jwks_uri                  = "https://www.linkedin.com/oauth/openid/jwks"
+  }
+
+  attribute_mapping = {
+    username = "sub"
+    email    = "email"
+  }
+}
+
+# Create a security group for the RDS cluster
 resource "aws_security_group" "db_sg" {
-  name        = "db_security_group"
-  description = "Allow access to RDS database"
+  name        = "aurora_db_security_group"
+  description = "Allow access to Aurora DB cluster"
 
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   egress {
@@ -80,4 +169,29 @@ resource "aws_security_group" "db_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Create the Aurora DB cluster
+resource "aws_rds_cluster" "aurora_cluster" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-postgresql"
+  engine_version          = "13.6"
+  master_username         = "myadmin"
+  master_password         = "mypassword"
+  database_name           = "mydatabase"
+  backup_retention_period = 7  # Retain backups for 7 days
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true 
+  vpc_security_group_ids  = [aws_security_group.db_sg.id]
+}
+
+# Create two Aurora DB cluster instances (Writer and Reader)
+resource "aws_rds_cluster_instance" "aurora_instance_writer" {
+  identifier              = "aurora-instance-1"
+  cluster_identifier      = aws_rds_cluster.aurora_cluster.id
+  instance_class          = "db.r6g.large"
+  engine                  = aws_rds_cluster.aurora_cluster.engine
+  engine_version          = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible     = false
+  apply_immediately       = true
 }
